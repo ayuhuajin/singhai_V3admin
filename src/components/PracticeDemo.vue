bbb8bfb4397533af97efd8b651d6dd5044644bec
wangqinghai <455493143@qq.com>
2023-05-10 10:35:48 +0800

    引入unplugin-auto-import 并配置解决ts Cannot find name 错误

--- src/components/PracticeDemo.vue
+++ src/components/PracticeDemo.vue
@@ -1,10 +1,15 @@
 <template>
   练习场
+  {{ count }}
+  {{ doubled }}
 
   <RouterLink to="/">Home</RouterLink>
   <RouterLink to="/about">About</RouterLink>
 </template>
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
+
+const count = ref(22277777)
+const doubled = computed(() => count.value * 2)
 </script>
 <style scoped lang="scss"></style>
